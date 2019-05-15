34eaff0fb8963661e69edf0db28c7ecd27e7467f
Jake Park <jake@jehmi.com>
2019-05-15 17:08:50 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started
+
+JDK 11  
+Spring Boot 2.1.4.RELEASE  
+Kotlin 1.3.31
---
+++ src/main/kotlin/info/jakepark/springboot2kotlin/SpringBoot2KotlinApplication.kt
@@ -0,0 +1,11 @@
+package info.jakepark.springboot2kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBoot2KotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBoot2KotlinApplication>(*args)
+}
